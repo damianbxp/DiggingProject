e3c1e25c7e9b3c7c42aac38c5c5bbfdb96bc8eea
Damian Bartuś <damianb.xp@gmail.com>
2020-11-22 14:52:56 +0100

    Added mesh generator Generating front face of map using marching squares

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager: MonoBehaviour
+{
+    public int width, height;
+    public float squareSize = 1;
+
+    private int[,] map;
+
+    MeshGenerator meshGenerator;
+
+    private void Start() {
+        meshGenerator = GetComponent<MeshGenerator>();
+        GenerateMap();
+        meshGenerator.GenerateMesh(map, squareSize);
+    }
+
+
+    void GenerateMap() {
+        map = new int[width, height];
+
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                map[i, j] = 1;
+            }
+        }
+    }
+}
